8beee5ddb2501f81f086c772603d57d8ba9834c8
chengyingzhan <albert.cheng@pdabc.com>
2019-11-01 17:47:43 +0800

    response_to_input

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -7,28 +7,55 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: 'Flutter Tutorial', home: MyButton()));
+  runApp(MaterialApp(title: 'Flutter Tutorial', home: Counter()));
 }
 
-class MyButton extends StatelessWidget {
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: Container(
-        height: 36.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(35.0),
-          color: Colors.purple[500],
-        ),
-        child: Center(
-          child: Text('Engage'),
-        ),
-      ),
+    return Row(
+      children: <Widget>[
+        CounterIncrementor(onPressed: _increment),
+        CounterDisplay(count: _counter),
+      ],
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count:$count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
     );
   }
 }
