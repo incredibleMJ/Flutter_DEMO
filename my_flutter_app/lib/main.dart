d4beea196b7f2bd6f5b482e02f5a671cd4f9bfc4
chengyingzhan <albert.cheng@pdabc.com>
2019-11-01 17:44:07 +0800

    Handling_gestures

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -7,38 +7,27 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Tutorial',
-    home: TutorialHome(),
-  ));
+  runApp(MaterialApp(title: 'Flutter Tutorial', home: MyButton()));
 }
 
-class TutorialHome extends StatelessWidget {
+class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(35.0),
+          color: Colors.purple[500],
         ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          )
-        ],
-      ),
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add',
-        child: Icon(Icons.add),
-        onPressed: null,
+        child: Center(
+          child: Text('Engage'),
+        ),
       ),
     );
   }
